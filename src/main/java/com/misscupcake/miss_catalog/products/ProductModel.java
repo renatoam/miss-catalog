99d9e0ea3047aed125ac587b0d5bf0773fbe1d2b
Renato <renato.melo@ab-inbev.com>
2024-08-01 21:16:13 -0300

    feat(1.2): create specification model

--- src/main/java/com/misscupcake/miss_catalog/products/ProductModel.java
+++ src/main/java/com/misscupcake/miss_catalog/products/ProductModel.java
@@ -6,6 +6,7 @@ import java.util.UUID;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -13,6 +14,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -29,8 +31,12 @@ public class ProductModel {
   private String description;
 
   @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
-  @OneToMany(mappedBy = "product", fetch = FetchType.LAZY)
+  @OneToMany(mappedBy = "product", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
   private Set<ProductImageModel> images = new HashSet<>();
+
+  @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+  @OneToOne(mappedBy = "product", cascade = CascadeType.ALL)
+  private ProductSpecificationModel specification;
 
   public UUID getId() {
     return id;
---
+++ src/main/java/com/misscupcake/miss_catalog/products/ProductSpecificationModel.java
@@ -0,0 +1,140 @@
+package com.misscupcake.miss_catalog.products;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "product_specification")
+public class ProductSpecificationModel {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private UUID id;
+
+  @Column(name = "product_id", nullable = false)
+  @OneToOne
+  private ProductModel product;
+
+  @Column(name = "size")
+  private String size;
+
+  @Column(name = "flavor")
+  private String flavor;
+
+  @Column(name = "weight")
+  private BigDecimal weight;
+
+  @Column(name = "combo_quantity")
+  private Short comboQuantity;
+
+  @Column(name = "frosting")
+  private String frosting;
+
+  @Column(name = "ingredients")
+  private String ingredients;
+
+  @Column(name = "shelf_life")
+  private Short shelfLife;
+  
+  @Column(name = "packaging")
+  private String packaging;
+
+  @Column(name = "availability")
+  private String availability;
+
+  public UUID getId() {
+    return id;
+  }
+
+  public void setId(UUID id) {
+    this.id = id;
+  }
+
+  public ProductModel getProduct() {
+    return product;
+  }
+
+  public void setProduct(ProductModel product) {
+    this.product = product;
+  }
+
+  public String getSize() {
+    return size;
+  }
+
+  public void setSize(String size) {
+    this.size = size;
+  }
+
+  public String getFlavor() {
+    return flavor;
+  }
+
+  public void setFlavor(String flavor) {
+    this.flavor = flavor;
+  }
+
+  public BigDecimal getWeight() {
+    return weight;
+  }
+
+  public void setWeight(BigDecimal weight) {
+    this.weight = weight;
+  }
+
+  public Short getComboQuantity() {
+    return comboQuantity;
+  }
+
+  public void setComboQuantity(Short comboQuantity) {
+    this.comboQuantity = comboQuantity;
+  }
+
+  public String getFrosting() {
+    return frosting;
+  }
+
+  public void setFrosting(String frosting) {
+    this.frosting = frosting;
+  }
+
+  public String getIngredients() {
+    return ingredients;
+  }
+
+  public void setIngredients(String ingredients) {
+    this.ingredients = ingredients;
+  }
+
+  public Short getShelfLife() {
+    return shelfLife;
+  }
+
+  public void setShelfLife(Short shelfLife) {
+    this.shelfLife = shelfLife;
+  }
+
+  public String getPackaging() {
+    return packaging;
+  }
+
+  public void setPackaging(String packaging) {
+    this.packaging = packaging;
+  }
+
+  public String getAvailability() {
+    return availability;
+  }
+
+  public void setAvailability(String availability) {
+    this.availability = availability;
+  }
+}
